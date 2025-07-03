4de0b945adca10b59e13ffbf8efe608bb3fd3dde
AlanCornejoQ <ecornejo@ucb.edu.bo>
2025-07-03 12:48:16 -0400

    arreglo comandos.py con control inventario

--- ordenes/comandos.py
+++ ordenes/comandos.py
@@ -1,19 +1,29 @@
+from inventario.gestor import GestorInventario
+
+# Base del patron Command
 class OrdenCommand:
     def ejecutar(self):
         raise NotImplementedError("Debes implementar ejecutar()")
 
-
-# Receptor: la orden real que se ejecuta
+# Receptor: representa una orden concreta
 class OrdenCompra:
     def __init__(self, producto, cantidad):
         self.producto = producto
         self.cantidad = cantidad
 
     def procesar(self):
-        return f"Orden procesada: {self.cantidad} x {self.producto.nombre}"
-    
+        inventario = GestorInventario.get_instancia()
+        nombre = self.producto.get_nombre()
 
-# Comando concreto
+        stock = inventario.stock.get(nombre, {}).get("cantidad", 0)
+
+        if stock >= self.cantidad:
+            inventario.stock[nombre]["cantidad"] -= self.cantidad
+            return f"Orden procesada: {self.cantidad} x {nombre}"
+        else:
+            return f"No hay stock suficiente para {nombre} (stock disponible: {stock})"
+
+# Comando concreto que ejecuta la orden
 class EjecutarOrden(OrdenCommand):
     def __init__(self, orden):
         self.orden = orden
